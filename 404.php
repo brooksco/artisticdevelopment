6324a6a40af6f803196aa6fcdf51b00f70ed9c73
Colin Brooks <colinbooks@gmail.com>
2016-04-21 23:44:18 -0400

    updated bearror

---
+++ 404.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>What is lost can be found again (404)</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style type="text/css">
+
+    body { 
+      background-color: #242b2b; 
+      color: white; 
+      font-family: arial, sans-serif;
+      margin: 0;
+    }
+
+    h2 { 
+      font-size: 4em; 
+      margin: 0; 
+      color: white; 
+
+    }
+
+    p { 
+      font-size: 1.5em; 
+      line-height: 1.5em;
+      margin: .5em 0em 0em 0em;
+    }
+
+    #bear-bearrier {
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      -webkit-box-align: center;
+
+      align-items: center;
+
+      height: 100vh;
+      max-width: 900px;
+      margin-left: auto; 
+      margin-right: auto;
+      padding: 0 2em 0 2em;
+    }
+
+    #andy {
+      width: 250px;
+      float: left;
+      display: inline;
+      margin: 0em 2em .5em 0em;
+    }
+
+    #somewhere {
+      text-decoration: none;
+      color: white;
+    }
+
+    #somewhere:hover {
+      text-decoration: underline;
+    }
+
+    @media (max-width: 620px) {
+      #bear-bearrier {
+        margin-top: 1.2em;
+        display: block;
+        height: auto;
+      }
+
+      #bearror-of-bad-news {
+        margin: 1.2em 0 1.2em 0;
+      }
+
+      #andy {
+        display: block;
+        float: none;
+        margin: auto;
+      }
+
+      h2 {
+        font-size: 3em;
+      }
+
+      p {
+        font-size: 1.1em;
+      }
+    }
+
+  </style>
+</head>
+
+<body>
+
+  <div id="bear-bearrier">
+   <img id="andy" src="img/404littlebear.png" />
+
+   <div id="bearror-of-bad-news">
+    <h2>Ruh roh.</h2>
+    <p>The page that maybe should be here seems to not be here. You might have mistyped something, or the page might have become lost. But remember, we are <em>alive</em>.</p>
+  </div>
+</div>
+
+</body>
+</html>
